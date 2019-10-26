2a53acdf3e9a582c1f9a3afdc3ca2ddfe173b478
andy_hardy <aohardy@gmail.com>
2019-10-25 23:38:53 -0600

    added build to git

---
+++ client/build/precache-manifest.ac69c4d81a2c578d820f788413e7ce2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f04303f674aa599c25529c3ba340f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af06f67bdb32b9d70ab5",
+    "url": "/static/css/2.d3161135.chunk.css"
+  },
+  {
+    "revision": "6eede80225c5f0ccff48",
+    "url": "/static/css/main.0a4db320.chunk.css"
+  },
+  {
+    "revision": "af06f67bdb32b9d70ab5",
+    "url": "/static/js/2.808e22c3.chunk.js"
+  },
+  {
+    "revision": "6eede80225c5f0ccff48",
+    "url": "/static/js/main.4495ebc6.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
